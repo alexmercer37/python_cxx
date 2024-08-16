3573422cafd5f165148ec086722e52c9aea2e6bc
alexmercer37 <3450141407@qq.com>
2024-08-16 15:18:11 +0800

    cxx_python

--- src/main.cpp
+++ src/main.cpp
@@ -18,73 +18,55 @@ std::shared_ptr<cv::Mat> matBuff;
 std::shared_ptr<cv::Mat> Buff;
 
 camera *camera_ptr;
-
-// void *k4aUpdate(void *camera_ptr)
-// {
-//     camera *Camera = (camera *)camera_ptr;
-
-//     while (1)
-//     {
-//         pthread_mutex_lock(&protect_picture);
-
-//         Camera->picture_update(capture);
-
-//         rgb_ptr = Camera->getpicture(capture, rgb_frame, k4aTransformation);
-//         cv::cvtColor(*rgb_ptr, *rgb_ptr, cv::COLOR_RGB2BGR);
-
-//         pthread_mutex_lock(&buff_mutex);
-//         matBuff = std::make_shared<cv::Mat>(rgb_ptr->clone());
-//         pthread_mutex_unlock(&buff_mutex);
-
-//         pthread_mutex_unlock(&protect_picture);
-
-//         usleep(1000);
-
-//         if (matBuff->empty())
-//         {
-//             cout << "error" << endl;
-//         }
-
-//         rgb_ptr->release();
-//     }
-//     pthread_exit(NULL);
-// }
-cv::Mat *camera::captureFrameFromCamera(int &cameraId)
-{
-    cv::VideoCapture cap(cameraId);
-    cv::Mat frame;
-
-    cv::Mat *frame_ptr = new cv::Mat;
-    frame_ptr = (cv::Mat *)cap.read(frame);
-    imshow("读取视频2", *frame_ptr);
-    cv::waitKey(1);
-    return frame_ptr;
-}
-
 void *init_kinect1(void *camera_ptr)
 {
     camera *Camera = (camera *)camera_ptr;
-    int cameraId = 2;
+
     while (1)
     {
         pthread_mutex_lock(&protect_picture);
-        rgb_ptr = Camera->captureFrameFromCamera(cameraId);
-        // pthread_mutex_lock(&buff_mutex);
 
-        // Buff = std::make_shared<cv::Mat>(rgb_ptr->clone());
-        cout << "ok" << endl;
-        // pthread_mutex_unlock(&buff_mutex);
+        Camera->picture_update(capture);
+
+        rgb_ptr = Camera->getpicture(capture, rgb_frame, k4aTransformation);
+        cv::cvtColor(*rgb_ptr, *rgb_ptr, cv::COLOR_RGB2BGR);
+
+        pthread_mutex_lock(&buff_mutex);
+        matBuff = std::make_shared<cv::Mat>(rgb_ptr->clone());
+        pthread_mutex_unlock(&buff_mutex);
+
         pthread_mutex_unlock(&protect_picture);
+
         usleep(1000);
-        if (Buff->empty())
+
+        if (matBuff->empty())
         {
             cout << "error" << endl;
         }
-        imshow("读取视频2", *rgb_ptr);
-        cv::waitKey(1);
+
         rgb_ptr->release();
     }
     pthread_exit(NULL);
+}
+
+void *k4aUpdate(void *argc)
+{
+    cv::VideoCapture cap(0);
+    cv::Mat frame;
+    while (1)
+    {
+        pthread_mutex_lock(&buff_mutex);
+
+        bool status = cap.read(frame);
+
+        Buff = std::make_shared<cv::Mat>(frame.clone());
+
+        pthread_mutex_unlock(&buff_mutex);
+        // cv::imshow("test", *Buff);
+        // cv::waitKey(1);
+
+        frame.release();
+    }
 }
 void *detect_python(void *argc)
 {
@@ -129,6 +111,17 @@ void *detect_python(void *argc)
             PyObject *detect_args = PyTuple_Pack(2, model, mat);             // 传入的参数
             PyObject *result = PyObject_CallObject(detect_fun, detect_args);
 
+            // if (PyList_Check(result))
+            // {
+            //     float SizeOfList = PyList_Size(result);
+            //     for (int i = 0; i < SizeOfList; i++)
+            //     {
+            //         PyObject *ListItem = PyList_GetItem(result, i);
+            //         cout << ListItem << endl;
+            //         Py_DECREF(ListItem);
+            //     }
+            // }
+
             Py_XDECREF(mat);
             Py_XDECREF(result);
             Py_XDECREF(detect_args);
@@ -137,24 +130,44 @@ void *detect_python(void *argc)
         }
     }
 }
+void *detect_python2(void *argc)
+{
+    Py_Initialize();
+    _import_array();
+
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("import os");
+    PyRun_SimpleString("sys.path.append(\'/home/dxy/Downloads/yolov5-6.0_two_labels\')");
+    PyRun_SimpleString("sys.path.append(\'.\')");
+    PyRun_SimpleString("import cv2");
+    PyRun_SimpleString("import numpy");
+    PyRun_SimpleString("import torch");
+    PyRun_SimpleString("import time");
+    PyRun_SimpleString("import serial");
+
+    PyObject *m_PyModule = PyImport_ImportModule("Detect");
+    PyObject *m_PyDict = PyModule_GetDict(m_PyModule);
+    PyObject *load_model = PyDict_GetItemString(m_PyDict, "load_model");
+    PyObject *model = PyObject_CallObject(load_model, NULL);
+}
 
 int main()
 {
 
     camera_ptr = new camera;
 
-    // camera_ptr->init_kinect(capture, k4aTransformation, k4aCalibration);
-
     pthread_mutex_init(&protect_picture, nullptr);
     pthread_mutex_init(&buff_mutex, nullptr);
 
     matBuff.reset(new cv::Mat);
     Buff.reset(new cv::Mat);
-
+    camera_ptr->init_kinect(capture, k4aTransformation, k4aCalibration);
     pthread_t tid[4];
     // pthread_create(&tid[0], NULL, k4aUpdate, (void *)camera_ptr);
-    pthread_create(&tid[0], NULL, init_kinect1, NULL);
+    pthread_create(&tid[0], NULL, init_kinect1, (void *)camera_ptr);
     pthread_create(&tid[1], NULL, detect_python, NULL);
+    // sleep(10);
+    // pthread_create(&tid[2], NULL, detect_python2, NULL);
 
     while (1)
         ;
